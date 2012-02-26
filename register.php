681dae1072d3db508c3aea1cd427205a68bd9e79
root <root@HCL@home.org>
2012-02-26 12:12:50 +0530

    Sample registration page - uses same base-style css.

--- register.php
+++ register.php
@@ -1,5 +1,39 @@
 <html>
-<h1>Registration Page[Optional]</h1>
+    <head>
+        <title>Genie - Registration</title>
+        <link href="./style/base-style.css" rel="stylesheet" type="text/css" />
+    </head>
 
-Registration in a separate page like this or use AJAX to show register window in the home page itself.
+    <body>
+        <div>    
+	    <form id="stylized" name="home" method="link" action="index.php">
+	        <button type="submit">Back Home</button>
+	    </form>    	    
+	</div>
+	                
+        <div id="stylized" class="login-box">    
+            <form id="form" name="form" method="post" action="validate_registration.php">
+            
+                <label>Name:</label>
+                <input type="text" name="name" id="name">
+                
+                <label>Username:</label>
+                <input type="text" name="uname" id="uname">
+
+                <label>Password:</label>
+                <input type="text" name="pwd1" id="pwd1">
+
+                <label>Confirm Password:</label>
+                <input type="text" name="pwd2" id="pwd2">
+
+                <label>Email:</label>
+                <input type="text" name="email" id="email">
+
+                <label>Mobile:</label>
+                <input type="text" name="mob" id="mob">
+                     
+                <button type="submit">Register</button>        
+            </form>
+        </div>
+    </body>
 </html>
